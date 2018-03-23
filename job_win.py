1510e06713c3b85b864966de73fb24944de19687
lixx11 <lxx2011011580@gmail.com>
2018-03-23 19:33:25 +0800

    finish basic job window

--- job_win.py
+++ job_win.py
@@ -20,13 +20,19 @@ class JobWindow(QWidget):
         _dir = dirname(abspath(__file__))
         self.workdir.setText(_dir)
 
+        col_count = self.job_table.columnCount()
+        self.header = {
+            x: self.job_table.horizontalHeaderItem(x).text() \
+            for x in range(col_count)
+        }
+        self.crawler_running = False
+
         # batch jobs
         self.curr_jobs = []
 
         # job window
         self.browse_btn.clicked.connect(self.select_workdir)
-        self.crawler_start_btn.clicked.connect(self.start_crawler)
-        self.crawler_stop_btn.clicked.connect(self.stop_crawler)
+        self.crawler_btn.clicked.connect(self.start_or_stop_crawler)
         self.job_table.customContextMenuRequested.connect(self.show_job_menu)
 
     # job window
@@ -37,46 +43,75 @@ class JobWindow(QWidget):
         self.workdir.setText(directory)
 
     @pyqtSlot()
-    def start_crawler(self):
-        self.crawler_start_btn.setEnabled(False)
-        workdir = self.workdir.text()
-        self.crawler_thread = CrawlerThread(workdir=workdir)
-        self.crawler_thread.jobs.connect(self.update_jobs)
-        self.crawler_thread.start()
-        self.crawler_stop_btn.setEnabled(True)
-
-    @pyqtSlot()
-    def stop_crawler(self):
-        self.crawler_start_btn.setEnabled(True)
-        self.crawler_thread.terminate()
-        self.crawler_stop_btn.setEnabled(False)
+    def start_or_stop_crawler(self):
+        if self.crawler_running:
+            self.crawler_running = False
+            self.crawler_btn.setText('start crawler')
+            self.crawler_thread.terminate()
+        else:
+            self.crawler_running = True
+            self.crawler_btn.setText('stop crawler')
+            workdir = self.workdir.text()
+            self.crawler_thread = CrawlerThread(workdir=workdir)
+            self.crawler_thread.jobs.connect(self.update_jobs)
+            self.crawler_thread.conf.connect(self.update_conf)
+            self.crawler_thread.start()
 
     @pyqtSlot(list)
     def update_jobs(self, jobs):
         job_table = self.job_table
-        print('%d jobs found' % len(jobs))
         curr_job_ids = [job['id'] for job in self.curr_jobs]
-        print(curr_job_ids)
         row_count = job_table.rowCount()
         for i in range(len(jobs)):
             job = jobs[i]
-            if job['id'] in curr_job_ids:
-                continue
-            job_table.insertRow(row_count + i)
-            item = QTableWidgetItem(job['name'])
-            job_table.setItem(i, 0, item)
+            self.fill_table_row(job, i)
             self.curr_jobs.append(job)
+
+    @pyqtSlot(list)
+    def update_conf(self, confs):
+        for conf in confs:
+            tag = os.path.basename(conf).split('.')[0]
+            self.hit_finding_conf.addItem(tag,userData=conf)
 
     @pyqtSlot(QPoint)
     def show_job_menu(self, pos):
         job_table = self.job_table
         menu = QMenu()
         row = job_table.currentRow()
-        job = job_table.item(row, 0)
+        job = job_table.item(row, 0).text()
+        workdir = self.workdir.text()
         action_h52cxi = menu.addAction('run h52cxi')
         action_hit_finding = menu.addAction('run hit finding')
         action = menu.exec_(job_table.mapToGlobal(pos))
         if action == action_h52cxi:
-            print('run h52cxi')
+            self.conv_thread = ConversionThread(
+                workdir=workdir,
+                job=job,
+                h5_dataset=self.h5_dataset.text(),
+                cxi_dataset=self.cxi_dataset.text(),
+            )
+            self.conv_thread.start()
         elif action == action_hit_finding:
-            print('run hit finding')
+            curr_id = self.hit_finding_conf.currentIndex()
+            conf = self.hit_finding_conf.itemData(curr_id)
+            self.hit_finding_thread = HitFindingThread(
+                workdir=workdir,
+                job=job,
+                conf=conf
+            )
+            self.hit_finding_thread.start()
+
+    def fill_table_row(self, row_dict, row):
+        row_count = self.job_table.rowCount()
+        if row_count == row:
+            self.job_table.insertRow(row_count)
+        for col, field in self.header.items():
+            item = self.job_table.item(row, col)
+            if item is None:
+                item = QTableWidgetItem(row_dict[field])
+                item.setTextAlignment(
+                    QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter
+                )
+                self.job_table.setItem(row, col, item)
+            else:
+                item.setText(str(row_dict[field]))
