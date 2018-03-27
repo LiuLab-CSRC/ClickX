07084cb9ab17c53ccc25a00f236485eeeebe9813
lixx11 <lxx2011011580@gmail.com>
2018-03-27 22:09:33 +0900

    update batch_h52cxi to batch_compressor

--- batch_compressor.py
+++ batch_compressor.py
@@ -1,16 +1,16 @@
 #!/bin/env python
 
-"""Convert h5 files to cxi files on multiple cores using MPI.
+"""Compress raw data in h5/cxi format to compressed cxi files on multiple cores using MPI.
 
 Usage:
-   batch_h52cxi.py <h5-lst> <h5-dataset> <cxi-dir> <cxi-lst-dir> [options]
+   batch_compression.py <in-lst> <in-dataset> <out-dir> <out-lst-dir> [options]
 
 Options:
     -h --help                   Show this screen.
     --compression COMP_FILTER   Specify compression filter [default: lzf].
-    --cxi-size SIZE             Specify max frame in a cxi file [default: 1000].
-    --cxi-dataset DATASET       Specify cxi dataset [default: data].
-    --cxi-dtype DATATYPE        Specify the datatype [default: uint16].
+    --out-size SIZE             Specify max frame in a cxi file [default: 1000].
+    --out-dataset DATASET       Specify cxi dataset [default: data].
+    --out-dtype DATATYPE        Specify the datatype [default: auto].
     --shuffle SHUFFLE           Whether use shuffle filter in compression [default: True]. 
     --batch-size SIZE           Specify batch size in a job [default: 50].
     --buffer-size SIZE          Specify buffer size in MPI communication
@@ -18,7 +18,8 @@ Options:
     --update-freq FREQ          Specify update frequency of progress [default: 10].
 """
 from mpi4py import MPI
-import numpy as np
+import h5py
+
 from util import save_cxi
 import time
 
@@ -27,69 +28,69 @@ import os
 from glob import glob
 from docopt import docopt
 import yaml
+from tqdm import tqdm
 
 
-def get_size(obj, seen=None):
-    """Recursively finds size of objects"""
-    size = sys.getsizeof(obj)
-    if seen is None:
-        seen = set()
-    obj_id = id(obj)
-    if obj_id in seen:
-        return 0
-    # Important mark as seen *before* entering recursion to gracefully handle
-    # self-referential objects
-    seen.add(obj_id)
-    if isinstance(obj, dict):
-        size += sum([get_size(v, seen) for v in obj.values()])
-        size += sum([get_size(k, seen) for k in obj.keys()])
-    elif hasattr(obj, '__dict__'):
-        size += get_size(obj.__dict__, seen)
-    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_size(i, seen) for i in obj])
-    return size
-
-
-def collect_jobs(files, batch_size):
+def collect_jobs(files, dataset, batch_size):
     jobs = []
     batch = []
-    total_h5 = 0
-    for f in files:
-        batch.append(f)
-        total_h5 += 1
-        if len(batch) == batch_size:
-            jobs.append(batch)
-            batch = []
+    frames = 0
+    print('collecting jobs...')
+    for i in tqdm(range(len(files))):
+        try:
+            shape = h5py.File(files[i], 'r')[dataset].shape
+            if len(shape) == 3:
+                nb_frame = shape[0]
+                for j in range(nb_frame):
+                    batch.append(
+                        {'filepath': files[i], 'dataset': dataset, 'frame': j}
+                    )
+                    frames += 1
+                    if len(batch) == batch_size:
+                        jobs.append(batch)
+                        batch = []
+            else:
+                batch.append(
+                    {'filepath': files[i], 'dataset': dataset, 'frame': j}
+                )
+                frames += 1
+                if len(batch) == batch_size:
+                    jobs.append(batch)
+                    batch = []
+        except OSError:
+            print('Failed to load %s' % files[i])
+            pass
     if len(batch) > 0:
         jobs.append(batch)
-    return jobs, total_h5
+    return jobs, frames
 
 
 def master_run(args):
-    # mkdir for output
-    cxi_dir = args['<cxi-dir>']
-    cxi_lst_dir = args['<cxi-lst-dir>']
-    if not os.path.isdir(cxi_dir):
-        os.makedirs(cxi_dir)
-    if not os.path.isdir(cxi_lst_dir):
-        os.makedirs(cxi_lst_dir)
+    # mkdir for output if not exist
+    out_dir = os.path.abspath(args['<out-dir>'])
+    out_lst_dir = os.path.abspath(args['<out-lst-dir>'])
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    if not os.path.isdir(out_lst_dir):
+        os.makedirs(out_lst_dir)
     # get all h5 files
-    h5_lst = args['<h5-lst>']
-    with open(h5_lst) as f:
+    in_lst = args['<in-lst>']
+    with open(in_lst) as f:
         _files = f.readlines()
     # remove trailing '/n'
     files = []
-    h5_raw_size = 0
+    in_size = 0  # total size fo input files(raw data)
     for f in _files:
         files.append(f[:-1])
-        h5_raw_size += os.path.getsize(f[:-1])
+        in_size += os.path.getsize(f[:-1])
     # collect jobs
     batch_size = int(args['--batch-size'])
     buffer_size = int(args['--buffer-size'])
-    jobs, total_frame = collect_jobs(files, batch_size)
-    total_jobs = len(jobs)
+    in_dataset = args['<in-dataset>']
+    jobs, nb_frames = collect_jobs(files, in_dataset, batch_size)
+    nb_jobs = len(jobs)
     time_start = time.time()
-    print('%d frames, %d jobs to be processed' % (total_frame, total_jobs))
+    print('%d frames, %d jobs to be processed' % (nb_frames, nb_jobs))
 
     update_freq = int(args['--update-freq'])
 
@@ -101,18 +102,18 @@ def master_run(args):
     for slave in slaves:
         comm.isend(jobs[job_id], dest=slave)
         reqs[slave] = comm.irecv(buf=buffer_size, source=slave)
-        print('job %d/%d sent to %d' % (job_id, total_jobs, slave))
+        print('job %d/%d sent to %d' % (job_id, nb_jobs, slave))
         sys.stdout.flush()
         job_id += 1
-    while job_id < total_jobs:
+    while job_id < nb_jobs:
         stop = False
         time.sleep(0.1)  # take a break
         for slave in slaves:
             finished, result = reqs[slave].test()
             if finished:
                 results += result
-                if job_id < total_jobs:
-                    print('job %d/%d sent to %d' % (job_id, total_jobs, slave))
+                if job_id < nb_jobs:
+                    print('job %d/%d sent to %d' % (job_id, nb_jobs, slave))
                     sys.stdout.flush()
                     comm.isend(stop, dest=slave)
                     comm.isend(jobs[job_id], dest=slave)
@@ -122,19 +123,19 @@ def master_run(args):
                     stop = True
                     comm.isend(stop, dest=slave)
         if job_id % update_freq == 0:
-            progress = float(job_id) / total_jobs * 100
+            progress = float(job_id) / nb_jobs * 100
             stat_dict = {
                 'time start': time_start,
                 'progress': '%.2f%%' % progress,
                 'duration/sec': 'not finished',
-                'total frames': total_frame,
-                'total jobs': total_jobs,
-                'h5 raw size': 'not finished',
-                'cxi raw size': 'not finished',
+                'total frames': nb_frames,
+                'total jobs': nb_jobs,
+                'raw size': 'not finished',
+                'compressed size': 'not finished',
                 'compression ratio': 'not finished',
             }
 
-            stat_file = os.path.join(cxi_dir, 'stat.yml')
+            stat_file = os.path.join(out_dir, 'stat.yml')
             with open(stat_file, 'w') as f:
                 yaml.dump(stat_dict, f, default_flow_style=False)
 
@@ -170,30 +171,29 @@ def master_run(args):
     time_end = time.time()
     duration = time_end - time_start
 
-    # write cxi lst file
-    cxi_raw_size = 0
-    cxi_prefix = os.path.basename(h5_lst).split('.')[0]
-    cxi_lst_file = open(os.path.join(cxi_lst_dir, '%s.lst' % cxi_prefix), 'w')
-    cxi_files = glob('%s/*.cxi' % cxi_dir)
-    for cxi_file in cxi_files:
-        cxi_file = os.path.abspath(cxi_file)
-        cxi_raw_size += os.path.getsize(cxi_file)
-        cxi_lst_file.write('%s\n' % cxi_file)
-    cxi_lst_file.close()
+    # write compressed cxi file lst
+    compressed_size = 0
+    prefix = os.path.basename(in_lst).split('.')[0]
+    out_lst_file = open(os.path.join(out_lst_dir, '%s.lst' % prefix), 'w')
+    out_files = glob('%s/*.cxi' % out_dir)
+    for out_file in out_files:
+        compressed_size += os.path.getsize(os.path.abspath(out_file))
+        out_lst_file.write('%s\n' % out_file)
+    out_lst_file.close()
 
-    compression_ratio = float(h5_raw_size) / float(cxi_raw_size)
+    compression_ratio = float(in_size) / float(compressed_size)
     stat_dict = {
         'time start': time_start,
         'progress': 'done',
         'duration/sec': duration,
-        'total frames': total_frame,
-        'total jobs': total_jobs,
-        'h5 raw size': h5_raw_size,
-        'cxi raw size': cxi_raw_size,
+        'total frames': nb_frames,
+        'total jobs': nb_jobs,
+        'raw size': in_size,
+        'compressed size': compressed_size,
         'compression ratio': '%.2f' % compression_ratio,
     }
     
-    stat_file = os.path.join(cxi_dir, 'stat.yml')
+    stat_file = os.path.join(out_dir, 'stat.yml')
     with open(stat_file, 'w') as f:
         yaml.dump(stat_dict, f, default_flow_style=False)
 
@@ -202,49 +202,41 @@ def master_run(args):
 
 def slave_run(args):
     stop = False
-    h5_lst = args['<h5-lst>']
-    h5_dataset = args['<h5-dataset>']
-    cxi_dataset = args['--cxi-dataset']
-    cxi_dir = args['<cxi-dir>']
-    cxi_size = int(args['--cxi-size'])
+    in_lst = args['<in-lst>']
+    out_dir = args['<out-dir>']
+    out_dataset = args['--out-dataset']
+    out_size = int(args['--out-size'])
     buffer_size = int(args['--buffer-size'])
-    cxi_prefix = os.path.basename(h5_lst).split('.')[0]
-    if args['--cxi-dtype'] == 'uint16':
-        cxi_dtype = np.uint16
-    elif args['--cxi-dtype'] == 'int16':
-        cxi_dtype = np.int16
-    else:
-        cxi_dtype = np.int32
+    prefix = os.path.basename(in_lst).split('.')[0]
+    out_dtype = args['--out-dtype']
     if args['--shuffle'] == 'True':
         shuffle = True
     else:
         shuffle = False
 
-    # perform h52cxi conversion
-    cxi_batch = []
-    cxi_count = 0
+    # perform compression
+    batch = []
+    count = 0
     while not stop:
         job = comm.recv(buf=buffer_size, source=0)
         for i in range(len(job)):
-            cxi_batch.append(job[i])
-            if len(cxi_batch) == cxi_size:
-                output = os.path.join(
-                    cxi_dir, 
-                    '%s-rank%d-job%d.cxi' % (cxi_prefix, rank, cxi_count)
+            batch.append(job[i])
+            if len(batch) == out_size:
+                out_file = os.path.join(
+                    out_dir, '%s-rank%d-job%d.cxi' % (prefix, rank, count)
                 )
-                save_cxi(cxi_batch, h5_dataset, cxi_dataset, output, shuffle=shuffle, cxi_dtype=cxi_dtype)
+                save_cxi(batch, out_file, out_dataset, out_dtype=out_dtype, shuffle=shuffle)
                 sys.stdout.flush()
-                cxi_batch.clear()
-                cxi_count += 1
+                batch.clear()
+                count += 1
         comm.send(job, dest=0)
         stop = comm.recv(source=0)
-    # save last cxi batch
-    if len(cxi_batch) > 0:
-        output = os.path.join(
-            cxi_dir, 
-            '%s-rank%d-job%d.cxi' % (cxi_prefix, rank, cxi_count)
+    # save last cxi batch if not empty
+    if len(batch) > 0:
+        out_file = os.path.join(
+            out_dir, '%s-rank%d-job%d.cxi' % (prefix, rank, count)
         )
-        save_cxi(cxi_batch, h5_dataset, cxi_dataset, output, shuffle=shuffle)
+        save_cxi(batch, out_file, out_dataset, out_dtype=out_dtype, shuffle=shuffle)
         sys.stdout.flush()
     done = True
     comm.send(done, dest=0)
@@ -254,7 +246,7 @@ if __name__ == '__main__':
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     if size == 1:
-        print('Run batch h52cxi with at leat 2 processs!')
+        print('Run batch compressor with at least 2 processes!')
         sys.exit()
 
     rank = comm.Get_rank()
